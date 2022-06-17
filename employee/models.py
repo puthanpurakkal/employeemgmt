6c7b466dc445592c3bad20bc6fdff3d5ded47408
karthikeyan <karthikeyankarthik908@gmail.com>
2022-06-17 11:14:16 +0530

    employee

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    eid=models.CharField(max_length=120,primary_key=True)
+    employee_name=models.CharField(max_length=120)
+    designation=models.CharField(max_length=120)
+    salary=models.PositiveIntegerField()
+    email=models.EmailField(unique=True)
+    experience=models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return self.employee_name
